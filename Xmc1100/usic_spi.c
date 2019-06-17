baeacf05a335a04e48cd5db004ca1a03c4d99c48
Lukas <xlukas9@gmail.com>
2019-06-17 22:39:44 +0200

    repo fix commit

--- Xmc1100/usic_spi.c
+++ Xmc1100/usic_spi.c
@@ -26,6 +26,17 @@ void spi_init(void){
 	SPI_MASTER_SetBaudRate(&SPI_MASTER_0,16000);
 }
 
+uint8 usic_spi_send_read( uint8* data_to_send, uint8* data_to_read, uint8 data_send_count , uint8 data_read_count){
+	SPI_MASTER_Transmit(&SPI_MASTER_0, data_to_send, data_send_count);
+	while(SPI_MASTER_IsTxBusy(&SPI_MASTER_0));
+	//wait for receive
+	//this delay is demanded, otherviwe it not read anything
+	delay_ms(50);
+	SPI_MASTER_Receive(&SPI_MASTER_0, data_to_read, data_read_count);
+	while(SPI_MASTER_IsRxBusy(&SPI_MASTER_0));
+	return 0;
+
+}
 
 void spi_bme_init(void){
 	SPI_MASTER_0 = bme_spi;
@@ -37,16 +48,4 @@ void spi_oled_init(void){
 	spi_init();
 }
 
-uint8 usic_spi_send_read( uint8* data_to_send, uint8* data_to_read, uint8 data_send_count , uint8 data_read_count){
-
-	SPI_MASTER_Transmit(&SPI_MASTER_0, data_to_send, data_send_count);
-	while(SPI_MASTER_IsTxBusy(&SPI_MASTER_0));
-	//wait for receive
-	//this delay is demanded, otherviwe it not read anything
-	delay_ms(40);
-	SPI_MASTER_Receive(&SPI_MASTER_0, data_to_read, data_read_count);
-	while(SPI_MASTER_IsRxBusy(&SPI_MASTER_0));
-	return 0;
-
-}
 
